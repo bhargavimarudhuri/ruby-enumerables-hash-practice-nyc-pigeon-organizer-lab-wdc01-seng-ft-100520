e3a42b3a2855bb91e2e8971ed6d49ece631e46d4
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-10-02 04:10:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,10 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   result = {}
   
-  # 
+  # color
   data.each {|key, valueArr| 
     puts "#{key} is #{valueArr}"
+    # color
     valueArr.each { |valKey, piegonArr|
       puts "#{valKey} is #{piegonArr}"
       piegonArr.each { |piegon|
